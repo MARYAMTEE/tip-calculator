609d694321d137d1e860c64a1fc529ea0b69b902
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-06-04 14:46:02 +0100

    update js file

--- script.js
+++ script.js
@@ -26,3 +26,38 @@ billInput.addEventListener('input', () => {
   bill = parseFloat(billInput.value);
   calculate();
 });
+
+const errorMessage = document.querySelector("#error-message");
+// When People input changes
+peopleInput.addEventListener("input", ()=> {
+    people = parseInt(peopleInput.value);
+    if(people <= 0 || isNaN(people)){
+        errorMessage.style.display = "inline";
+        errorMessage.textContent = "Can't be zero"
+        tipAmountDisplay.textContent = `0.00`;
+        totalAmountDisplay.textContent = `0.00`;
+    }else{
+        errorMessage.style.display = "none";
+    calculate();
+    }
+});
+
+// When a tip button is clicked
+tipButtons.forEach(button => {
+    button.addEventListener("click", ()=> {
+        tipButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+    
+    tipValue = parseInt(button.getAttribute("data-tip"));
+    customTipInput.value = ''; //clear custom input if a button is clicked
+    calculate();
+    });
+});
+
+//When custom tip is typed
+customTipInput.addEventListener("input", () => {
+    tipButtons.forEach(btn => btn.classList.remove("active")); //deselect buttons
+    tipValue = parseFloat(customTipInput.value);
+    calculate();
+});
